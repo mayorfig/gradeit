42ac4d107ff991174d879d59dd39daf81b00ed3b
Daniel Coker <danielcoker28@outlook.com>
2024-01-09 01:17:46 +0100

    Add matric number, name, and created on fields

--- grader/models.py
+++ grader/models.py
@@ -16,3 +16,6 @@ class Essay(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField(max_length=100000)
     score = models.IntegerField(null=True, blank=True)
+    matric_no = models.CharField(max_length=20, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
