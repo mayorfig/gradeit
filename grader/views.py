59ec0416fa3219c6d7a8bb2d92b0403fd7817a4f
Daniel Coker <danielcoker28@outlook.com>
2024-01-09 01:18:09 +0100

    Update views and templates

--- grader/views.py
+++ grader/views.py
@@ -9,50 +9,67 @@ from .utils.model import *
 from .utils.helpers import *
 
 import os
+
 current_path = os.path.abspath(os.path.dirname(__file__))
+
 
 # Create your views here.
 def index(request):
-    questions_list = Question.objects.order_by('set')
+    questions_list = Question.objects.order_by("set")
     context = {
-        'questions_list': questions_list,
+        "questions_list": questions_list,
     }
-    return render(request, 'grader/index.html', context)
+    return render(request, "grader/index.html", context)
+
 
 def responses(request):
-    questions_list = Question.objects.order_by('set')
+    essay_list = Essay.objects.select_related("question").all().order_by("-created_at")
+
     context = {
-        'questions_list': questions_list,
+        "essay_list": essay_list,
     }
-    return render(request, 'grader/responses.html', context)
+
+    return render(request, "grader/responses.html", context)
+
 
 def essay(request, question_id, essay_id):
     essay = get_object_or_404(Essay, pk=essay_id)
     context = {
         "essay": essay,
     }
-    return render(request, 'grader/essay.html', context)
+    return render(request, "grader/essay.html", context)
+
 
 def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         # create a form instance and populate it with data from the request:
         form = AnswerForm(request.POST)
         if form.is_valid():
-
-            content = form.cleaned_data.get('answer')
+            content = form.cleaned_data.get("answer")
+            matric_no = form.cleaned_data.get("matric")
+            name = form.cleaned_data.get("name")
 
             if len(content) > 20:
                 num_features = 300
-                model = word2vec.KeyedVectors.load_word2vec_format(os.path.join(current_path, "deep_learning_files/word2vec.bin"), binary=True)
+                model = word2vec.KeyedVectors.load_word2vec_format(
+                    os.path.join(current_path, "deep_learning_files/word2vec.bin"),
+                    binary=True,
+                )
                 clean_test_essays = []
-                clean_test_essays.append(essay_to_wordlist( content, remove_stopwords=True ))
-                testDataVecs = getAvgFeatureVecs( clean_test_essays, model, num_features )
+                clean_test_essays.append(
+                    essay_to_wordlist(content, remove_stopwords=True)
+                )
+                testDataVecs = getAvgFeatureVecs(clean_test_essays, model, num_features)
                 testDataVecs = np.array(testDataVecs)
-                testDataVecs = np.reshape(testDataVecs, (testDataVecs.shape[0], 1, testDataVecs.shape[1]))
+                testDataVecs = np.reshape(
+                    testDataVecs, (testDataVecs.shape[0], 1, testDataVecs.shape[1])
+                )
 
                 lstm_model = get_model()
-                lstm_model.load_weights(os.path.join(current_path, "deep_learning_files/final_lstm.h5"))
+                lstm_model.load_weights(
+                    os.path.join(current_path, "deep_learning_files/final_lstm.h5")
+                )
                 preds = lstm_model.predict(testDataVecs)
 
                 if math.isnan(preds):
@@ -71,9 +88,11 @@ def question(request, question_id):
             essay = Essay.objects.create(
                 content=content,
                 question=question,
-                score=preds
+                score=preds,
+                matric_no=matric_no,
+                name=name,
             )
-        return redirect('essay', question_id=question.set, essay_id=essay.id)
+        return redirect("essay", question_id=question.set, essay_id=essay.id)
     else:
         form = AnswerForm()
 
@@ -81,4 +100,4 @@ def question(request, question_id):
         "question": question,
         "form": form,
     }
-    return render(request, 'grader/question.html', context)
+    return render(request, "grader/question.html", context)
